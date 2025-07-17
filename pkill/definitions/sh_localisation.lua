0778face088dc9c656c32e016438e88735c3fe74
bloodycop <89155277+bloodycop6385@users.noreply.github.com>
2025-07-18 02:11:47 +0300

    Update sh_localisation.lua

--- pkill/definitions/sh_localisation.lua
+++ pkill/definitions/sh_localisation.lua
@@ -17,3 +17,9 @@ ax.localisation:Register("pt-PT", {
     ["config.permakill.enabled.help"] = "Quando ativado, os personagens dos jogadores serão permanentemente mortos ao morrer, impedindo o ressurgimento.",
     ["config.permakill"] = "Morte Permanente",
 })
+
+ax.localisation:Register("es-ES", {
+    ["config.permakill.enabled"] = "Habilitar Muerte Permanente",
+    ["config.permakill.enabled.help"] = "Cuando está habilitado, los personajes de los jugadores serán asesinados permanentemente al morir, impidiendo la reaparición.",
+    ["config.permakill"] = "Muerte Permanente",
+})
